58b0451f39990bcd8d0b4564285b2c4625a4cc7f
yositaka.koizumi <yositaka.koizumi@esco.co.jp>
2025-01-13 20:55:20 +0900

    途中保存

--- lambdas/resize/src/index.ts
+++ lambdas/resize/src/index.ts
@@ -46,7 +46,23 @@ export const handler: SQSHandler = async (event: SQSEvent) => {
 
       const uploadKey = `${PROCESS}/${parsedKey.name}-${PROCESS}${parsedKey.ext}`
 
-      await putImageToS3(s3Client, bucketName, uploadKey, imageBuffer);
+      await putImageToS3(s3Client, bucketName, uploadKey, imageBuffer)
+
+      // 4. send message to sqs
+      const s3Message: S3Message = { bucketName, key: uploadKey }
+      // sqs client
+      const sqsClient = new SQSClient()
+      // sqs command
+      const input: SendMessageCommandInput = {
+        QueueUrl: QUEUE_URL,
+        MessageBody: JSON.stringify(s3Message),
+      }
+      const command: SendMessageCommand = new SendMessageCommand(input)
+      // send
+      await sqsClient.send(command)
+      console.log(
+        `sent the message to SQS, message: ${JSON.stringify(s3Message)}`
+      )
     }
   }
 }
