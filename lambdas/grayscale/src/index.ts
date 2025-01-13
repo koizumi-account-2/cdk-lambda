6f50bd7d73c01d35deaa529e85ae02312bcc6132
yositaka.koizumi <yositaka.koizumi@esco.co.jp>
2025-01-14 01:41:39 +0900

    grayscaleのlambdaの作成

---
+++ lambdas/grayscale/src/index.ts
@@ -0,0 +1,33 @@
+import {SQSEvent, SQSHandler} from 'aws-lambda'
+import * as path from "path"
+
+import {getImageFromS3, putImageToS3} from "../../common/src"
+import { S3Client } from '@aws-sdk/client-s3';
+import {S3Message} from "../../common/src/types"
+
+
+const PROCESS = "grayscale";
+
+//SQSはbodyをStringで提供する
+
+export const handler:SQSHandler = async (event:SQSEvent)=>{
+    const s3Client = new S3Client();
+    for(const record of event.Records){
+        const message = record.body;
+        const s3Message:S3Message = JSON.parse(message)
+        const bucketName = s3Message.bucketName;
+        const key = s3Message.key;
+        const parsedKey = path.parse(key);
+        // download
+        const image = await getImageFromS3(s3Client,bucketName,key);
+        // edit
+        image.grayscale();
+        // upload
+        const uploadKey = `${PROCESS}/${parsedKey.name}-${PROCESS}${parsedKey.ext}`;
+        const imageBuffer = await image.getBufferAsync(image.getMIME());
+        console.log(`uploadKey:${uploadKey}, bucket:${bucketName}`);
+        await putImageToS3(s3Client,bucketName,uploadKey,imageBuffer);
+
+
+    }
+}
