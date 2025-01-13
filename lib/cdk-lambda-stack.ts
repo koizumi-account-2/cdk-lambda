551608a4dfbb5312f8e1a39b822208e6da42937f
yositaka.koizumi <yositaka.koizumi@esco.co.jp>
2025-01-13 10:52:22 +0900

    lamdaのdeploy

--- lib/cdk-lambda-stack.ts
+++ lib/cdk-lambda-stack.ts
@@ -1,7 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from "aws-cdk-lib/aws-s3"
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as path from 'path';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
+const REPOSITORY_TOP=path.join(__dirname,"../");
 const PREFIX = "cdk-lambda-koizumi-test"
 
 
@@ -14,5 +18,16 @@ export class CdkLambdaStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY
     })
 
+    const resizeLambda = new NodejsFunction(this,`${PREFIX}-lambda-resize`,{
+      functionName: `${PREFIX}-resize`,
+      entry: path.join(REPOSITORY_TOP,"lambdas/resize/src/index.ts"),
+      handler: "handler",
+      runtime: lambda.Runtime.NODEJS_22_X,
+      memorySize: 128,
+      timeout: cdk.Duration.seconds(30)
+    })
+
   }
+
+
 }
