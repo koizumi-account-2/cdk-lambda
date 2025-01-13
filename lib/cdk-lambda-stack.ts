e2bdfd92370ffc0078bea59da66d4806f693a475
yositaka.koizumi <yositaka.koizumi@esco.co.jp>
2025-01-13 11:45:41 +0900

    微調整

--- lib/cdk-lambda-stack.ts
+++ lib/cdk-lambda-stack.ts
@@ -4,9 +4,12 @@ import * as s3 from "aws-cdk-lib/aws-s3"
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as path from 'path';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { LambdaDestination } from 'aws-cdk-lib/aws-s3-notifications';
+
 
 const REPOSITORY_TOP=path.join(__dirname,"../");
 const PREFIX = "cdk-lambda-koizumi-test"
+
 
 
 export class CdkLambdaStack extends cdk.Stack {
@@ -26,6 +29,17 @@ export class CdkLambdaStack extends cdk.Stack {
       memorySize: 128,
       timeout: cdk.Duration.seconds(30)
     })
+
+    bucket.grantPut(resizeLambda)
+    bucket.grantReadWrite(resizeLambda)
+
+    bucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED,
+      new LambdaDestination(resizeLambda),
+      {
+        prefix: "original/"
+      }
+    );
 
   }
 
